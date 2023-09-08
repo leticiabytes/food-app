02f43452e8c9b9790b7a22f04a0df2e9e188df7d
leticiabytes <lvieira268@gmail.com>
2023-09-08 20:07:25 -0300

    feat: creates data to foods
    Signed-off-by: leticiabytes <lvieira268@gmail.com>

---
+++ src/components/Foods/data.ts
@@ -0,0 +1,86 @@
+export const foodData = [
+  {
+    id: 1,
+    title: "Red n Hot Pizza",
+    price: "$ 17,00",
+    image:
+      "https://images.unsplash.com/photo-1594007654729-407eedc4be65?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1928&q=80",
+  },
+  {
+    id: 2,
+    title: "Greek Salad",
+    price: "$ 22,00",
+    image:
+      "https://plus.unsplash.com/premium_photo-1673590981689-2ae5af3fa31f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80",
+  },
+  {
+    id: 3,
+    title: "Chicken Hawaiian",
+    price: "$ 16,00",
+    image:
+      "https://images.unsplash.com/photo-1626074353765-517a681e40be?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80",
+  },
+  {
+    id: 4,
+    title: "Ground Beef Tacos",
+    price: "$ 11,00",
+    image:
+      "https://images.unsplash.com/photo-1686555758112-f2ed8567f1f4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80",
+  },
+  {
+    id: 5,
+    title: "Beef Burguer",
+    price: "$ 12,00",
+    image:
+      "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1998&q=80",
+  },
+  {
+    id: 6,
+    title: "Chicken Curry with Rice",
+    price: "$ 126,00",
+    image:
+      "https://images.unsplash.com/photo-1627662234691-53b28f5a680c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80",
+  },
+  {
+    id: 7,
+    title: "Chocolate Cake",
+    price: "$ 63,00",
+    image:
+      "https://images.unsplash.com/photo-1606890737304-57a1ca8a5b62?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2003&q=80",
+  },
+  {
+    id: 7,
+    title: "Hot Dog",
+    price: "$ 5,00",
+    image:
+      "https://images.unsplash.com/photo-1612392166886-ee8475b03af2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80",
+  },
+  {
+    id: 8,
+    title: "French Fries",
+    price: "$ 12,00",
+    image:
+      "https://images.unsplash.com/photo-1585109649139-366815a0d713?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+  },
+  {
+    id: 9,
+    title: "Cheese Burger",
+    price: "$ 18,00",
+    image:
+      "https://images.unsplash.com/photo-1584178639036-613ba57e5e39?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+  },
+  {
+    id: 10,
+    title: "Coke",
+    price: "$ 2,00",
+    image:
+      "https://images.unsplash.com/photo-1596803244618-8dbee441d70b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80",
+  },
+  {
+    id: 11,
+    title: "Ice Cream",
+    price: "$ 8,00",
+    image:
+      "https://images.unsplash.com/photo-1497034825429-c343d7c6a68f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80",
+  },
+];
