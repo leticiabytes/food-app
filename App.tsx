4d1d681389163c7ece07297e0eae87350d0f2159
leticiabytes <lvieira268@gmail.com>
2023-09-10 10:34:30 -0300

    feat: add navigation to app index
    Signed-off-by: leticiabytes <lvieira268@gmail.com>

--- App.tsx
+++ App.tsx
@@ -1,21 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import Home from "./src/screens/Home";
+import { AppNavigation } from "./src/navigation";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Home />
-      <StatusBar style="dark" />
-    </View>
-  );
+  return <AppNavigation />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
