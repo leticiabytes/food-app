94b7d0e2a55c18a37d82d74860462f8b33b9030b
leticiabytes <lvieira268@gmail.com>
2023-09-10 10:33:32 -0300

    feat: add navigation config
    Signed-off-by: leticiabytes <lvieira268@gmail.com>

---
+++ src/navigation/index.tsx
@@ -0,0 +1,19 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Home from "../screens/Home";
+
+const Stack = createNativeStackNavigator();
+
+export function AppNavigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
