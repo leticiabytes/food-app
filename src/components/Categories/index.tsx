38cc3454ec03c84ce9a631a834eca54ff8b0dd0f
leticiabytes <lvieira268@gmail.com>
2023-09-10 14:17:42 -0300

    feat: add adjustments styles
    Signed-off-by: leticiabytes <lvieira268@gmail.com>

--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -34,7 +34,10 @@ export function Categories({
               className="flex items-center justify-center space-y-1"
             >
               <View
-                className={"rounded-full p-2 w-12 h-12" + activeButtonClass}
+                className={
+                  "items-center justify-center rounded-full p-2 w-12 h-12" +
+                  activeButtonClass
+                }
               >
                 <Text className="text-2xl">{item.image}</Text>
               </View>
