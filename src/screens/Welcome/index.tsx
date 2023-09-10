78d865e72451f4363cf0a96e30215955efe10e45
leticiabytes <lvieira268@gmail.com>
2023-09-10 14:56:39 -0300

    feat: finish welcome screen
    Signed-off-by: leticiabytes <lvieira268@gmail.com>

--- src/screens/Welcome/index.tsx
+++ src/screens/Welcome/index.tsx
@@ -8,29 +8,36 @@ import { View, Text, Image } from "react-native";
 import { RootStackParamList } from "../../navigation";
 
 export default function Welcome() {
+  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+
   const ring1padding = useSharedValue(0);
   const ring2padding = useSharedValue(0);
-
-  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
 
   useEffect(() => {
     ring1padding.value = 0;
     ring2padding.value = 0;
     setTimeout(
-      () => (ring1padding.value = withSpring(ring1padding.value + 18)),
+      () => (ring1padding.value = withSpring(ring1padding.value + 20)),
       100
     );
     setTimeout(
-      () => (ring2padding.value = withSpring(ring2padding.value + 19)),
+      () => (ring2padding.value = withSpring(ring2padding.value + 50)),
       300
     );
 
     setTimeout(() => navigation.navigate("Home"), 2500);
   }, []);
   return (
-    <View className="flex-1 justify-center items-center space-y-10 bg-amber-500">
+    <View className="flex-1 justify-center items-center space-y-10 bg-orange-500">
       <StatusBar style="light" />
-
+      <View className="flex items-center space-y-2">
+        <Text className="text-4xl font-bold text-white tracking-widest">
+          Fast Food
+        </Text>
+        <Text className="font-medium text-center text-white tracking-widest max-w-[220px]">
+          Your favourite foods delivered fast at your door.
+        </Text>
+      </View>
       <Animated.View
         className="bg-white/20 rounded-full"
         style={{ padding: ring2padding }}
@@ -40,18 +47,13 @@ export default function Welcome() {
           style={{ padding: ring1padding }}
         >
           <Image
-            source={require("../../../assets/images/welcome.png")}
-            style={{ width: 60, height: 60 }}
+            // resizeMode="contain"
+
+            source={require("../../../assets/images/Pizza.png")}
+            style={{ width: 300, height: 300 }}
           />
         </Animated.View>
       </Animated.View>
-
-      <View className="flex items-center space-y-2">
-        <Text className="font-bold text-white tracking-widest">Foody</Text>
-        <Text className="font-medium text-white tracking-widest">
-          Food is always right
-        </Text>
-      </View>
     </View>
   );
 }
