291f35e1b87ee4f8474a4b3a1badba055c7a1f8e
leticiabytes <lvieira268@gmail.com>
2023-09-10 14:18:07 -0300

    feat: finish foods component
    Signed-off-by: leticiabytes <lvieira268@gmail.com>

--- src/components/Foods/index.tsx
+++ src/components/Foods/index.tsx
@@ -1,32 +1,43 @@
 import { View, Text, Pressable, Image } from "react-native";
+import { useNavigation, NavigationProp } from "@react-navigation/native";
 import MasonryList from "@react-native-seoul/masonry-list";
 
+import { RootStackParamList } from "../../navigation";
+import { truncatedName } from "../../utils/truncatedName";
+
 import { IFood } from "../../interfaces/IFood";
-import { ICategory } from "../../interfaces/ICategory";
 
 type FoodProps = {
   foods: IFood[];
-  categories: ICategory[];
 };
 
-export function Foods({ categories, foods }: FoodProps) {
+type FoodCardProps = {
+  item: IFood;
+  index: number;
+};
+
+export function Foods({ foods }: FoodProps) {
   return (
     <View className="mx-4 space-y-3">
-      <Text className="font-semibold text-neutral-600">Recipes</Text>
+      <Text className="font-semibold text-neutral-600 text-xl">Foods</Text>
       <MasonryList
         data={foods}
         keyExtractor={(item) => item.id}
         numColumns={2}
         showsVerticalScrollIndicator={false}
-        renderItem={({ item, i }) => <RecipeCard item={item} index={i} />}
+        renderItem={({ item, i }) => (
+          <FoodCard item={item as IFood} index={i} />
+        )}
         onEndReachedThreshold={0.1}
       />
     </View>
   );
 }
 
-const RecipeCard = ({ item, index }: any) => {
-  let isEven = index % 2 == 0;
+const FoodCard = ({ item, index }: FoodCardProps) => {
+  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+  const isEven = index % 2 === 0;
+
   return (
     <Pressable
       style={{
@@ -34,23 +45,27 @@ const RecipeCard = ({ item, index }: any) => {
         paddingLeft: isEven ? 0 : 8,
         paddingRight: isEven ? 8 : 0,
       }}
+      onPress={() => navigation.navigate("FoodDetails", { ...item })}
       className="flex justify-center mb-2 "
     >
-      <Image
-        source={{ uri: item.image }}
-        style={{
-          width: "100%",
-          height: index % 3 === 0 ? 150 : 255,
-          borderRadius: 16,
-        }}
-        className="bg-black/5"
-        // sharedTransitionTag={item.title}
-      />
-      <View className="flex justify-between flex-row mt-1 mb-2">
+      <View className="">
+        <Image
+          source={{ uri: item.image }}
+          style={{
+            width: "100%",
+            height: index % 3 === 0 ? 150 : 255,
+            borderRadius: 16,
+          }}
+          className="bg-black/5"
+        />
+        <View className="absolute bg-white/90 rounded-full p-2 mt-2 right-2">
+          <Text className="text-amber-500 font-bold">$ {item.price}</Text>
+        </View>
+      </View>
+      <View className="mt-1 mb-2">
         <Text className="font-semibold mt-1 text-neutral-600">
-          {item.name.length > 20 ? item.name.slice(0, 20) + "..." : item.name}
+          {truncatedName(item.name)}
         </Text>
-        <Text className="text-amber-500 mt-1 weight-bold">{item.price}</Text>
       </View>
     </Pressable>
   );
