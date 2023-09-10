12853e97057af6574b243b3fe441d9ac1a7ca8d9
leticiabytes <lvieira268@gmail.com>
2023-09-10 14:17:16 -0300

    feat: finish food details screen
    Signed-off-by: leticiabytes <lvieira268@gmail.com>

--- src/screens/FoodDetails/index.tsx
+++ src/screens/FoodDetails/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import Animated, { FadeInDown, FadeIn } from "react-native-reanimated";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
 
 import { StatusBar } from "expo-status-bar";
 import { ScrollView, Image, View, TouchableOpacity, Text } from "react-native";
@@ -10,105 +11,140 @@ import {
   HeartIcon,
   MinusIcon,
   PlusIcon,
+  ShoppingBagIcon,
 } from "react-native-heroicons/outline";
 import {
   HeartIcon as HeartIconSolid,
   StarIcon,
 } from "react-native-heroicons/solid";
 
-export function FoodDetails() {
+import { IFood } from "../../interfaces/IFood";
+
+import { AddOn } from "../../components/AddOn";
+
+export default function FoodDetails(props: NativeStackScreenProps<any, any>) {
   const navigation = useNavigation();
+  const item = props.route.params as IFood;
 
   const [isFavorite, setIsFavorite] = useState(false);
   const [countUnit, setCountUnit] = useState(1);
 
   return (
-    <ScrollView
-      className="bg-white flex-1"
-      showsVerticalScrollIndicator={false}
-      contentContainerStyle={{ paddingBottom: 30, paddingHorizontal: 16 }}
-    >
-      <StatusBar style={"dark"} />
-
-      <View className="flex-row justify-center">
-        <Image
-          source={{
-            uri: "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1998&q=80",
-          }}
-          resizeMode="center"
-          style={{
-            width: "100%",
-            height: 250,
-            borderRadius: 10,
-            marginTop: 48,
-          }}
-        />
-      </View>
-
-      <Animated.View
-        entering={FadeIn.delay(200).duration(1000)}
-        className="w-full absolute flex-row justify-between items-center pt-14 mt-2"
+    <View className="bg-white flex-1">
+      <ScrollView
+        className="flex-1"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 30, paddingHorizontal: 16 }}
       >
-        <TouchableOpacity
-          onPress={() => navigation.goBack()}
-          className="p-2 rounded-full ml-8 bg-white"
-        >
-          <ChevronLeftIcon size={28} color="gray" />
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => setIsFavorite(!isFavorite)}
-          className="p-2 rounded-full  bg-white"
-        >
-          {isFavorite ? (
-            <HeartIconSolid size={28} color="red" />
-          ) : (
-            <HeartIcon size={28} color="grey" />
-          )}
-        </TouchableOpacity>
-      </Animated.View>
+        <StatusBar style={"dark"} />
 
-      <View className="flex justify-between space-y-4 pt-3">
+        <View className="flex-row justify-center">
+          <Image
+            source={{
+              uri: item.image,
+            }}
+            resizeMode="center"
+            style={{
+              width: "100%",
+              height: 250,
+              borderRadius: 10,
+              marginTop: 48,
+            }}
+          />
+        </View>
+
         <Animated.View
-          entering={FadeInDown.duration(700).springify().damping(12)}
-          className="space-y-2"
+          entering={FadeIn.delay(200).duration(1000)}
+          className="w-full absolute flex-row justify-between items-center pt-14 mt-2"
         >
-          <Text className="text-3xl font-bold flex-1 text-primary">
-            Buguer Mesh
-          </Text>
-
-          <View className="flex-row items-center gap-x-2">
-            <StarIcon size={20} color="#FFC529" />
-
-            <View className="flex-row">
-              <Text className="font-bold">4.5</Text>
-              <Text className="text-neutral-400 ml-2">(30+)</Text>
-              <Text className="text-amber-500 ml-2 text underline">
-                See Review
-              </Text>
-            </View>
-          </View>
-
-          <View className="pt-2 flex-row justify-between">
-            <Text className="font-bold text-2xl text-amber-500">$ 12.50</Text>
-
-            <View className="flex-row gap-x-4 items-center">
-              <TouchableOpacity
-                onPress={() => setCountUnit(countUnit - 1)}
-                className="p-2 rounded-full border-2 border-amber-500 bg-white"
-              >
-                <MinusIcon size={18} color="#FFC529" />
-              </TouchableOpacity>
-              <Text className="text-2xl">{countUnit}</Text>
-              <TouchableOpacity
-                onPress={() => setCountUnit(countUnit + 1)}
-                className="p-2 rounded-full bg-amber-500"
-              >
-                <PlusIcon size={18} color="white" />
-              </TouchableOpacity>
-            </View>
-          </View>
+          <TouchableOpacity
+            onPress={() => navigation.goBack()}
+            className="p-2 rounded-full ml-8 bg-white"
+          >
+            <ChevronLeftIcon size={28} color="gray" />
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => setIsFavorite(!isFavorite)}
+            className="p-2 rounded-full  bg-white"
+          >
+            {isFavorite ? (
+              <HeartIconSolid size={28} color="red" />
+            ) : (
+              <HeartIcon size={28} color="grey" />
+            )}
+          </TouchableOpacity>
         </Animated.View>
+
+        <View className="flex justify-between space-y-4 pt-3">
+          <Animated.View
+            entering={FadeInDown.duration(700).springify().damping(12)}
+            className="space-y-2"
+          >
+            <Text className="text-3xl font-bold flex-1 text-primary">
+              {item.name}
+            </Text>
+
+            <View className="flex-row items-center gap-x-2">
+              <StarIcon size={20} color="#FFC529" />
+
+              <View className="flex-row">
+                <Text className="font-bold">4.5</Text>
+                <Text className="text-neutral-400 ml-2">(30+)</Text>
+                <Text className="text-amber-500 ml-2 text underline">
+                  See Review
+                </Text>
+              </View>
+            </View>
+
+            <View className="pt-2 flex-row justify-between">
+              <Text className="font-bold text-2xl text-amber-500">
+                $ {item.price}
+              </Text>
+
+              <View className="flex-row gap-x-4 items-center">
+                <TouchableOpacity
+                  onPress={() => setCountUnit(countUnit - 1)}
+                  className="p-2 rounded-full border-2 border-amber-500 bg-white"
+                >
+                  <MinusIcon size={18} color="#FFC529" strokeWidth={4} />
+                </TouchableOpacity>
+                <Text className="text-2xl">{countUnit}</Text>
+                <TouchableOpacity
+                  onPress={() => setCountUnit(countUnit + 1)}
+                  className="p-2 rounded-full bg-amber-500"
+                >
+                  <PlusIcon size={18} color="white" strokeWidth={4} />
+                </TouchableOpacity>
+              </View>
+            </View>
+
+            <Text className="pt-4 text-neutral-400 text-md">
+              {item.description}
+            </Text>
+          </Animated.View>
+        </View>
+
+        <View className="mt-4">
+          <Text className="font-bold text-xl">Choice of Add On</Text>
+
+          <AddOn name="Pepper Julienned" price={2.3} image="🌶" />
+          <AddOn name="Salad Spinach" price={4.7} image="🥗" />
+          <AddOn name="Bacon" price={2.7} image="🥓" />
+        </View>
+      </ScrollView>
+
+      <View className="bottom-8 justify-center">
+        <View className="justify-center flex-row">
+          <View className="w-[167px] flex-row items-center rounded-full bg-amber-500 p-[6px]">
+            <View className="bg-white rounded-full p-3">
+              <ShoppingBagIcon size={18} color="#FFC529" />
+            </View>
+            <Text className="mb-1 text-white font-bold px-3 tracking-wider">
+              Add to Cart
+            </Text>
+          </View>
+        </View>
       </View>
-    </ScrollView>
+    </View>
   );
 }
