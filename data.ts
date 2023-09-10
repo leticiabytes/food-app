2b310d25043dc7c742ddfea9a5da18fb19ac6524
leticiabytes <lvieira268@gmail.com>
2023-09-09 21:47:31 -0300

    chore: create a data fake api
    Signed-off-by: leticiabytes <lvieira268@gmail.com>

---
+++ data.ts
@@ -0,0 +1,356 @@
+export const data = [
+  {
+    categories: [
+      {
+        id: 1,
+        name: "Burgers",
+        image: "🍔",
+        foods: [
+          {
+            id: 101,
+            name: "Cheeseburger",
+            price: 9.99,
+            image:
+              "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1998&q=80",
+          },
+          {
+            id: 102,
+            name: "Bacon Burger",
+            price: 10.99,
+            image:
+              "https://images.unsplash.com/photo-1553979459-d2229ba7433b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1968&q=80",
+          },
+          {
+            id: 103,
+            name: "Veggie Burger",
+            price: 8.99,
+            image:
+              "https://images.unsplash.com/photo-1610440042657-612c34d95e9f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80",
+          },
+          {
+            id: 104,
+            name: "X-Salad Burger",
+            price: 8.99,
+            image:
+              "https://images.unsplash.com/photo-1603064752734-4c48eff53d05?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1976&q=80",
+          },
+          {
+            id: 105,
+            name: "Doble Meat Burger",
+            price: 8.99,
+            image:
+              "https://plus.unsplash.com/premium_photo-1683619761492-639240d29bb5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80",
+          },
+          {
+            id: 106,
+            name: "Cream Cheese Burger",
+            price: 8.99,
+            image:
+              "https://images.unsplash.com/photo-1606755962773-d324e0a13086?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80",
+          },
+          {
+            id: 107,
+            name: "Mr. Chicken Burger",
+            price: 8.99,
+            image:
+              "https://images.unsplash.com/photo-1615297928064-24977384d0da?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2012&q=80",
+          },
+        ],
+      },
+      {
+        id: 2,
+        name: "Pizza",
+        image: "🍕",
+        foods: [
+          {
+            id: 201,
+            name: "Hawaiian Pizza",
+            price: 12.99,
+            image:
+              "https://images.unsplash.com/photo-1604382354936-07c5d9983bd3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+          },
+          {
+            id: 202,
+            name: "Pepperoni Pizza",
+            price: 10.99,
+            image:
+              "https://images.unsplash.com/photo-1628840042765-356cda07504e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2080&q=80",
+          },
+          {
+            id: 203,
+            name: "Margherita",
+            price: 4.99,
+            image:
+              "https://images.unsplash.com/photo-1594007654729-407eedc4be65?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1928&q=80",
+          },
+          {
+            id: 204,
+            name: "Vegetarian",
+            price: 4.99,
+            image:
+              "https://images.unsplash.com/photo-1593246049226-ded77bf90326?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1935&q=80",
+          },
+          {
+            id: 205,
+            name: "Chicken Pizza",
+            price: 4.99,
+            image:
+              "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1981&q=80",
+          },
+          {
+            id: 206,
+            name: "Cheese Pizza",
+            price: 4.99,
+            image:
+              "https://images.unsplash.com/photo-1571066811602-716837d681de?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1844&q=80",
+          },
+        ],
+      },
+      {
+        id: 3,
+        name: "Meat",
+        image: "🍗",
+        foods: [
+          {
+            id: 301,
+            name: "Pork Ribs",
+            price: 15.99,
+            image:
+              "https://images.unsplash.com/photo-1544025162-d76694265947?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2069&q=80",
+          },
+          {
+            id: 302,
+            name: "Beef Ribeye",
+            price: 9.99,
+            image:
+              "https://images.unsplash.com/photo-1676300185292-e23bb3db50fa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+          },
+          {
+            id: 303,
+            name: "Beef Ground",
+            price: 3.99,
+            image:
+              "https://images.unsplash.com/photo-1603360946369-dc9bb6258143?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1935&q=80",
+          },
+          {
+            id: 304,
+            name: "Pork Chop",
+            price: 3.99,
+            image:
+              "https://images.unsplash.com/photo-1612871689527-2a6145cc9e86?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80",
+          },
+          {
+            id: 305,
+            name: "Sausages",
+            price: 3.99,
+            image:
+              "https://images.unsplash.com/photo-1552913903-2cffa1962dc7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+          },
+        ],
+      },
+      {
+        id: 4,
+        name: "Donut",
+        image: "🍩",
+        foods: [
+          {
+            id: 401,
+            name: "Pink",
+            price: 15.99,
+            image:
+              "https://images.unsplash.com/photo-1544025162-d76694265947?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2069&q=80",
+          },
+          {
+            id: 402,
+            name: "Sugar",
+            price: 9.99,
+            image:
+              "https://images.unsplash.com/photo-1558745010-d2a3c21762ab?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80",
+          },
+          {
+            id: 403,
+            name: "Chocolate",
+            price: 3.99,
+            image:
+              "https://plus.unsplash.com/premium_photo-1672846027103-a50797886f99?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+          },
+        ],
+      },
+      {
+        id: 5,
+        name: "Mexican",
+        image: "🌮",
+        foods: [
+          {
+            id: 501,
+            name: "Guacamole",
+            price: 12.99,
+            image:
+              "https://plus.unsplash.com/premium_photo-1681406689584-2f7612fa98a4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80",
+          },
+          {
+            id: 502,
+            name: "Taco",
+            price: 10.99,
+            image:
+              "https://images.unsplash.com/photo-1565299585323-38d6b0865b47?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1980&q=80",
+          },
+          {
+            id: 503,
+            name: "Burrito",
+            price: 4.99,
+            image:
+              "https://images.unsplash.com/photo-1626700051175-6818013e1d4f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80",
+          },
+        ],
+      },
+      {
+        id: 6,
+        name: "Dessert",
+        image: "🧁",
+        foods: [
+          {
+            id: 601,
+            name: "Ice Cream",
+            price: 12.99,
+            image:
+              "https://images.unsplash.com/photo-1551024506-0bccd828d307?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80",
+          },
+          {
+            id: 602,
+            name: "Cupcake",
+            price: 10.99,
+            image:
+              "https://images.unsplash.com/photo-1563729784474-d77dbb933a9e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80",
+          },
+          {
+            id: 603,
+            name: "Cookie",
+            price: 4.99,
+            image:
+              "https://images.unsplash.com/photo-1499636136210-6f4ee915583e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80",
+          },
+          {
+            id: 604,
+            name: "Chocolate Cake",
+            price: 12.55,
+            image:
+              "https://images.unsplash.com/photo-1578985545062-69928b1d9587?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1989&q=80",
+          },
+          {
+            id: 605,
+            name: "Chocolate Cake",
+            price: 4.99,
+            image:
+              "https://images.unsplash.com/photo-1578985545062-69928b1d9587?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1989&q=80",
+          },
+          {
+            id: 606,
+            name: "Apple Pie",
+            price: 8.17,
+            image:
+              "https://images.unsplash.com/photo-1519915028121-7d3463d20b13?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80",
+          },
+          {
+            id: 606,
+            name: "Macaroons",
+            price: 2.0,
+            image:
+              "https://images.unsplash.com/photo-1656435571363-cc1ba6c10d00?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+          },
+        ],
+      },
+      {
+        id: 7,
+        name: "Asian",
+        image: "🍱",
+        foods: [
+          {
+            id: 701,
+            name: "Asama Laksa",
+            price: 12.99,
+            image:
+              "https://images.unsplash.com/photo-1632057153728-9ee20b615f8d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1931&q=80",
+          },
+          {
+            id: 702,
+            name: "Stinky Tofu",
+            price: 10.99,
+            image:
+              "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2080&q=80",
+          },
+          {
+            id: 703,
+            name: "Sushi",
+            price: 4.99,
+            image:
+              "https://images.unsplash.com/photo-1579584425555-c3ce17fd4351?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2127&q=80",
+          },
+          {
+            id: 703,
+            name: "Dim Sum",
+            price: 12.99,
+            image:
+              "https://plus.unsplash.com/premium_photo-1674601030943-9fb2d0c96721?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2080&q=80",
+          },
+        ],
+      },
+      {
+        id: 8,
+        name: "Drinks",
+        image: "🍹",
+        foods: [
+          {
+            id: 801,
+            name: "Coffee",
+            price: 2.99,
+            image:
+              "https://images.unsplash.com/photo-1541167760496-1628856ab772?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1937&q=80",
+          },
+          {
+            id: 802,
+            name: "Juice",
+            price: 1.99,
+            image:
+              "https://images.unsplash.com/photo-1600271886742-f049cd451bba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80",
+          },
+          {
+            id: 803,
+            name: "Beer",
+            price: 4.99,
+            image:
+              "https://images.unsplash.com/photo-1600788886242-5c96aabe3757?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80",
+          },
+          {
+            id: 803,
+            name: "Wine",
+            price: 12.99,
+            image:
+              "https://images.unsplash.com/photo-1553361371-9b22f78e8b1d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80",
+          },
+          {
+            id: 803,
+            name: "Whiskey Sour",
+            price: 12.99,
+            image:
+              "https://images.unsplash.com/photo-1470337458703-46ad1756a187?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2069&q=80",
+          },
+          {
+            id: 803,
+            name: "Margarita",
+            price: 17.99,
+            image:
+              "https://images.unsplash.com/photo-1551024709-8f23befc6f87?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2157&q=80",
+          },
+          {
+            id: 803,
+            name: "Martini",
+            price: 27.99,
+            image:
+              "https://images.unsplash.com/photo-1575023782549-62ca0d244b39?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80",
+          },
+        ],
+      },
+    ],
+  },
+];
